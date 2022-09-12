84defd7fc42161106af9a1d52816050168082816
mmamorim <marc.m.amorim@gmail.com>
2022-09-12 20:40:59 -0300

    antes do recreio

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 
-console.log("oi gente");
-console.log("oi gente"); 
+let y = 45
+
+function fazAlgo(y) {
+    const x = `oi 
+        "alguém" gente 
+        com ${y} anos
+    `
+    console.log(x)
+}
+
+
+if (y == 45) {
+    let x = 56
+    let z = "oi"
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
+
+fazAlgo(33)
+//console.log(x); //aqui a variavel x não existe!
+//console.log(z); //aqui a variavel z não existe!
+
+let idades = [ 10, true, 5.6, "oi gente", 21 ]
+idades.push(56)
+
+console.log(idades.length)
+
+//for(let i=0; i<10; i++) {
+//    console.log("passei aqui "+i);
+//}
